d83ba0afa9820c429b5dab11c83817ceb7af4e77
Ajaya K C <kshettryajaya@gmail.com>
2022-06-02 23:21:38 +1000

    Project Completed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain='dev-pu8wyk-g.us.auth0.com'
-      clientId='GYsVn60CkUU6fa4yiwpRrT1KVL5KuzPh'
+      domain='dev-26rdkt3b.us.auth0.com'
+      clientId='5qLZLXuTubUT1rZRvPujXFqLo9F0Yv9x'
       redirectUri={window.location.origin}
       cacheLocation='localstorage'
     >
